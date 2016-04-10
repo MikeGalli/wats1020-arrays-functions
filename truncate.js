117c5ca5155d8434b346843654a88bd4dd391c55
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-04-10 13:18:31 -0700

    msg

--- truncate.js
+++ truncate.js
@@ -12,7 +12,7 @@ var truncateWords = function(longText, numWords) {
   console.log("Check short str1 " + shortText);
   return shortText;
 }
-console.log('FFFFFFF' + shortText);
+//console.log('FFFFFFF' + shortText);
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
 console.log('originalText: ' + originalText);
