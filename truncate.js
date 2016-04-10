bc7e5666189b77d815a5159b0245bed3fb0b3ae2
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-04-10 13:53:49 -0700

    adfgh

--- truncate.js
+++ truncate.js
@@ -1,12 +1,20 @@
-//var shortText = "";
 var truncateWords = function (longText, numWords) {
     var shortTextArray = longText.split(" ").splice(0, wordLimit);
     shortTextArray.push('...');
-console.log('shortTextArray: ' + shortTextArray);
+              console.log('shortTextArray: ' + shortTextArray);
     shortText = shortTextArray.join(' ');
     return shortText;
-
 }
+var numWordsOrigText = originalText.split(' ').length;
+              console.log('The Number of Words in the Orginial Text is: ' + numWordsOrigText);
+
+
+
+
+
+
+
+
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
 var shortText = truncateWords(originalText, wordLimit);
