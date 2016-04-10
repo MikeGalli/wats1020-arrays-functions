41a1521e5cfcfac598dbd20232957d29e6bb32a1
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-04-10 13:01:11 -0700

    wdfb

--- truncate.js
+++ truncate.js
@@ -6,8 +6,7 @@ document.write("kljgadFKJHsdfkhjadsghjk")
 
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
-var shortText = truncateWords(originalText, wordLimit);
-console.log('originalText: ' + originalText);
+//console.log('originalText: ' + originalText);
 document.write('originalText: ' + originalText);
 console.log('shortText: ' + shortText);
 document.write('shortText: ' + shortText);
