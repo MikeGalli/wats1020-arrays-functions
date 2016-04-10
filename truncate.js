09b7a1ccd2aed8e5364f31184ac141acdbef6af6
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-04-10 12:52:45 -0700

    weta

--- truncate.js
+++ truncate.js
@@ -1,20 +1,22 @@
-// This is the file where you will write the Truncate Words function and related code.
+var truncateWords = function(longText, numWords) {
+  var shortTextArray = originalText.split(' ').splice(0, wordLimit)
+  console.log('Check_shortTextArray ' + shortTextArray);
 
-// TODO: Create a Function called truncateWords() that accepts two arguments:
-// longText (a String with several words in it)
-// numWords (an Integer that sets the number of words you want in the returned text)
+  shortTextArray.push('...');
+  console.log('Check_shortTextArray2 ' + shortTextArray);
 
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
+  shortText = shortTextArray.join(' ');
+  console.log("Check short str1 " + shortText);
+  shortText = shortText.slice(0,-4) + "...";
+  console.log("Check short str2 " + shortText);
+  return shortText;
+}
 
-// This portion of the script is meant to call and display the result of your Function.
-// You do not need to change the following lines, but you may change them if you
-// are experimenting further or pursuing stretch goals.
-console.log("98609860i9^G&&^(^)(&^&(&(*(&(&(^&%&$^##))))))");
-document.write("what the ^%&%$^&%&$&$&$$&");
+var originalTextLength = originalText.split(' ').length;
+console.log('Original Text Length is ' + originalTextLength);
+
+var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
+var wordLimit = 8;
+var shortText = truncateWords(originalText, wordLimit);
+console.log('originalText: ' + originalText);
+console.log('shortText: ' + shortText);
